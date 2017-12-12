b6804bfda0618f67021dc5fc9608e96ac2c2dee9
huangzhangjiang <1589016324@qq.com>
2017-12-12 18:07:12 +0800

    update 更新sql

--- DB.sql
+++ DB.sql
@@ -10,16 +10,22 @@ drop table if exists writers;
 create table writers(
   id int(11) primary key AUTO_INCREMENT,
   dynasty_id int(11),
-  name varchar(20)
+  name varchar(20) COMMENT '名字',
+  books varchar(1000) COMMENT '作品列表',
+  max_page int(11) COMMENT '最大页面',
+  information text COMMENT '简介'
 );
 
 drop table if exists sentence;
 create table sentence(
   id int(11) primary key AUTO_INCREMENT,
   writer_id int(11),
+  book varchar(50) COMMENT '书名',
+  star int(11),
   sentence varchar(1000)
 );
 
+delete from writers;
 insert into writers(dynasty_id, name) value(10, "三毛");
 
 delete from sentence;
